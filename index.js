e76d21fc9eff779d0e8c10b0f537cff9bd888bbd
murilokrugner <murilokrugner@hotmail.com>
2022-06-14 21:45:48 -0300

    setinterval

--- index.js
+++ index.js
@@ -1,20 +1,68 @@
-const puppeteer = require('puppeteer');
-const readlineSync = require('readline-sync');
+import puppeteer from "puppeteer";
+import readlineSync from "readline-sync";
+import ObjectsToCsv from "objects-to-csv";
+import neatCsv from "neat-csv";
+import fs from "fs";
 
 async function robo() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  const moedaBase = readlineSync.question('Informe uma moeda base: ') || 'dolar';
-  const moedaFinal =  readlineSync.question('Informe uma moeda desejada: ') || 'real';
+  const moedaBase = "bitcoin";
+  // readlineSync.question("Informe uma moeda base: ") || "dolar";
+  const moedaFinal = "real";
+  // readlineSync.question("Informe uma moeda desejada: ") || "real";
   const url = `https://www.google.com/search?q=${moedaBase}+para+${moedaFinal}&oq=${moedaBase}+para+${moedaFinal}&aqs=chrome.0.0i512l10.2646j1j7&sourceid=chrome&ie=UTF-8`;
   await page.goto(url);
-  
+
   const resultado = await page.evaluate(() => {
-    return document.querySelector('.a61j6.vk_gy.vk_sh.Hg3mWc').value;    
+    return document.querySelector(".cilsF.a61j6").value;
   });
 
   console.log(`O valor de 1 ${moedaBase} em ${moedaFinal} é: ${resultado}`);
- await browser.close();
+
+  const currentDate = new Date();
+  const day = currentDate.getDay();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+
+  const dateCompleted = day + "-" + month + "-" + year;
+
+  const hour = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+
+  const completeDay =
+    day + "/" + month + "/" + year + " " + hour + ":" + minutes;
+
+  const result = [
+    {
+      currentValue: completeDay + " " + resultado,
+    },
+  ];
+
+  const csv = new ObjectsToCsv(result);
+  await csv.toDisk(`${dateCompleted}.csv`, { append: true });
+  await browser.close();
 }
 
-robo();
+async function readValue() {
+  const currentDate = new Date();
+  const day = currentDate.getDay();
+  const month = currentDate.getMonth();
+  const year = currentDate.getFullYear();
+
+  const dateCompleted = day + "-" + month + "-" + year;
+
+  fs.readFile(`${dateCompleted}.csv`, async (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(await neatCsv(data));
+  });
+}
+
+setInterval(() => {
+  robo();
+}, 10000);
+
+//readValue();

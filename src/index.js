da2bbdb84fc8c94a71471372f8872b4eab3e7407
murilokrugner <murilokrugner@hotmail.com>
2022-06-15 21:42:59 -0300

    heroku deploy

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import cors from "cors";
 const app = express();
 const server = require("http").Server(app);
 
+const port = process.env.PORT || 3000;
+
 app.use(express.json());
 
 app.use(cors());
 
 app.use(routes);
 
-server.listen(21028);
+server.listen(port);
